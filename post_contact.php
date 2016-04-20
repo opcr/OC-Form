7886f4a79e7b44dd5de71725bb614ac60ad6f1b2
mlcassiau <mlcassiau@yahoo.fr>
2016-04-20 13:16:45 +0200

    ajouts de commentaires au fichier post_contact.php

--- post_contact.php
+++ post_contact.php
@@ -1,34 +1,51 @@
 <?php
 $errors = [];
 
+//SI RIEN N'EST RENSEIGNE AU NIVEAU DU PRENOM--> MESSAGE D'ERREUR
+
 if(!array_key_exists('firstname', $_POST) || $_POST['firstname'] == ''){
 	$errors['firstname'] = "Veuillez saisir votre prénom";
 }
+
+//SI RIEN N'EST RENSEIGNE AU NIVEAU DU NOM DE FAMILLE--> MESSAGE D'ERREUR
 
 if(!array_key_exists('lastname', $_POST) || $_POST['lastname'] == ''){
 	$errors['lastname'] = "Veuillez saisir votre nom de famille";
 }
 
+//SI RIEN N EST RENSEIGNE AU NIVEAU DE L'ADRESSE MAIL OU SI ELLE N'EST PAS VALIDE--> MESSAGE D'ERREUR
+
 if(!array_key_exists('adressmail', $_POST) || $_POST['adressmail'] == '' || !filter_var($_POST['adressmail'], FILTER_VALIDATE_EMAIL)){
 	$errors['adressmail'] = "Veuillez saisir un e-mail valide";
 }
 
+// SI LE MESSAGE N'EST PAS RENSEIGNE --> MESSAGE D'ERREUR
+
 if(!array_key_exists('message', $_POST) || $_POST['message'] == ''){
 	$errors['message'] = "Veuillez saisir un message";
 }
+// SI LE RECAPTCHA N'EST PAS RENSEIGNE OU S'IL EST MAL RENSEIGNE --> MESSAGE D'ERREUR
 if(!array_key_exists('g-recaptcha-response', $_POST) || $_POST['g-recaptcha-response'] == false){
 	$errors['g-recaptcha-response'] = "Veuillez valider le captcha";
 }
+
+// ON DEMARRE UNE SESSION
 session_start();
-	
+
+// S'IL Y A DES ERREURS, ON AFFICHE LES MESSAGES D'ERREURS DEFINIS PRECEDEMMENT
 if(!empty($errors)){
 	header('location: contact-oc-form.php');
 	$_SESSION['errors'] = $errors;
 	$_SESSION['inputs'] = $_POST;
-}else{
+}
+//SINON ON ENVOIE UN MAIL A L'ADRESSE QUE VOUS INDIQUEZ
+else{
 	header('location: contact-oc-form.php');
 	$_SESSION['success'] = 1;
+	
+	//ADRESSE MAIL QUE VOUS SOUHAITEZ:
 	$mailto = "info@dvwdesign.ch";
+	
 	$firstname = $_POST['firstname'];
 	$lastname = $_POST['lastname'];
 	$adressmail = $_POST['adressmail'];
